837cb3a38f002a90e350b31ca867787fa29d97ca
gjtinoco <genaro_13@hotmail.es>
2018-10-26 09:12:44 -0600

    Finalizacion Proyecto

--- src/components/Resultado/Resultado.js
+++ src/components/Resultado/Resultado.js
@@ -1,18 +1,23 @@
-import React,{PureComponent} from 'react';
+import React,{Component} from 'react';
 import {TransitionGroup, CSSTransition} from 'react-transition-group';
+import './Resultado.css';
 //react-transition-group
-class Resultado extends PureComponent {
+class Resultado extends Component {
 
     render() {
+
+        let resultado = this.props.resultado;
+        let mensaje = (!resultado) ? 'Elige Marcar, año, y Plan' : '$'+resultado;
         return (
             <div className="gran-total">
-                <TransitionGroup component="span" className="resultado">
+                <TransitionGroup
+                className="fade">
+
                     <CSSTransition
-                        className="resultado"
-                        key={this.props.resultado}
-                        timeout={{enter: 500, exit: 500}}
+                        classNames="fade"
+                        timeout={1500}
                         >
-                        <span>{ this.props.resultado }</span>
+                        <span>{mensaje}</span>
                     </CSSTransition>
                 </TransitionGroup>
             </div>
